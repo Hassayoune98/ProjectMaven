dffc8f794f397a405abb580903573fd129a6ae49
Hassayoune <hassayoune.98@gmail.com>
2019-03-11 08:53:42 +0100

    Webhook Test Commit

--- src/main/java/com/mycompany/maven/Test.java
+++ src/main/java/com/mycompany/maven/Test.java
@@ -10,5 +10,8 @@ package com.mycompany.maven;
  * @author Hassayoune
  */
 public class Test {
+    public static void main(String[] args) {
+        System.out.println("Test Test");
+    }
     
 }
